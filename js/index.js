e7167050ec4306a563925b34f85d67abfeb8ee52
kikutea330 <kikutea330@openmailbox.org>
2017-07-14 23:42:39 +0900

    init project

--- js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var menuMini = document.getElementById('menuMiniIcon');
-  menuMini.addEventListener('click',function(){
-    var menu = document.getElementById('menu');
-    var hiddenState = menu.getAttribute('aria-hidden');
-    menu.setAttribute('aria-hidden', hiddenState == 'false');
-  });
-});
